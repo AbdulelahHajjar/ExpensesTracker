431298f5df121e36093989b8ea6b8749c51b0bb9
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2020-08-25 01:34:10 +0300

    Initial Commit

---
+++ ExpensesTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExpensesTracker
+//
+//  Created by Abdulelah Hajjar on 25/08/2020.
+//  Copyright © 2020 Abdulelah Hajjar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
